ac80af10e2349b57ce31c25c770236b0aea5992e
zzzeee <linzy120@126.com>
2017-07-25 17:19:29 +0800

    saga

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
 import App from './components/App';
 import reducer from './reducers';
+import { helloSaga } from './sagas';
 
-const store = createStore(reducer);
+const store = createStore(
+	reducer,
+	applyMiddleware(createSagaMiddleware(helloSaga))
+);
 
 store.subscribe(()=>console.log(store.getState()));
 
